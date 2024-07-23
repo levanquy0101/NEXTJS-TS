827b9ed636b3f37dcadafe8acdb5c0cd50e47e0d
Le Van Quy <138227498+levanquy0101@users.noreply.github.com>
2024-07-23 15:13:05 +0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
   corePlugins: {
     preflight: false, // Tắt các CSS mặc định
   },
