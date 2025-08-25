da11bf91beb3ab7f6cb55245c502fc998b4c05e3
Le Van Quy <vannn.quy@gmail.com>
2025-08-25 11:36:51 +0700

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.scss'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main>
+          <Header />
           {children}
         </main>
       </body>
---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="bg-blue-500 p-4">
+      <h1 className="text-2xl font-bold text-white">Header</h1>
+    </div>
+  )
+}
